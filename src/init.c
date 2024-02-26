4f78d4079bf3c457798763573a89b915029c79b1
Zhang <panpan.zhang@vumc.org>
2024-02-26 15:05:54 -0600

    fix a typo in C

--- src/init.c
+++ src/init.c
@@ -27,7 +27,7 @@ static const R_CallMethodDef CallEntries[] = {
     {NULL, NULL, 0}
 };
 
-void R_init_wdnet(DllInfo *dll)
+void R_init_nonparlongdat(DllInfo *dll)
 {
     R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
     R_useDynamicSymbols(dll, FALSE);
